ecac3ad105873ea91885543aecffa81882f2b2a8
Вотчинников Павел Олегович <pavel.votchinnikov@rtkit.corp>
2023-08-07 15:28:28 +0300

    Кортежи

---
+++ src/06-tuples.ts
@@ -0,0 +1,10 @@
+const pairs: [string, string][] = [
+  ["key", "values"],
+  ["key2", "values2"],
+];
+
+const data: [number, boolean, string] = [1, true, "asd"];
+
+//csv
+const doc: [string, string, number, Date][] = [];
+doc.push(["Pasha", "Qwe", 24, new Date(1998, 11, 3)]);
