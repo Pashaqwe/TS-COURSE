af20a594d71b331bf00e3fc83e03809342cda826
Вотчинников Павел Олегович <pavel.votchinnikov@rtkit.corp>
2023-08-27 14:37:37 +0300

    Интерфейсы против алиасов(типов)

---
+++ src/SecondModule/03-interfaces-vs-aliases.ts
@@ -0,0 +1,11 @@
+// Aliases(type)
+// 1.primitives, unions, intersections
+/* 2. Мы не можем расширять существующие типы, но можем создать новый 
+type newWindow = Window & { a: "string" }; */
+
+// Interfaces
+/* 1. interface нужен, если мы хотим расширять существующий interface
+interface Window {
+  a: "string";
+} */
+// 2. Если мы пишем типы для classe, то мы используем interface
