2f687968cc989f23113ab9b310d6d57a6ba02cf9
Вотчинников Павел Олегович <pavel.votchinnikov@rtkit.corp>
2023-09-08 16:04:53 +0300

    Корректировка имени папок

---
+++ src/02-intermediate/10-exclamation-mark.ts
@@ -0,0 +1,23 @@
+let word: string | null = null;
+export const num = 10;
+
+if (num > 5) {
+  word = "abc";
+}
+
+console.log(word!.toLowerCase());
+
+function printName(name?: string) {
+  const fullName: string = name!;
+}
+
+interface Persone {
+  name: string;
+  age: number;
+}
+
+function printName2(persone?: Persone) {
+  console.log(persone!.name);
+}
+
+/* Использованеи этого символа не приветствуется */
